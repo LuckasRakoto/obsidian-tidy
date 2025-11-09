aad45b27926a2d7e1912da936820af66d828f11e
Luckas Rakoto <luckasrakoto@gmail.com>
2025-11-08 21:23:52 -0500

    Init cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::collections::HashMap;
+
+struct Cli {
+    root_path: std::path::PathBuf,
+}
+
+#[allow(dead_code, unused_mut)]
+fn find_all_images() -> HashMap<String, u8>{
+    let mut m = HashMap::new();
+    m
+}
+
 fn main() {
-    println!("Hello, world!");
+    let path = std::env::args().nth(1).expect("Specify root folder");
+    let args = Cli {
+        root_path: std::path::PathBuf::from(path),
+    };
+    println!("root : {:?}", args.root_path);
 }
